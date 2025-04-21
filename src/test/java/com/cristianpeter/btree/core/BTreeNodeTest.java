5288616445246da32cda7db07981fe77fc44029f
CristianPeter <horatiucristianpeter@gmail.com>
2025-04-21 21:58:37 +0200

    continue with BtreeNodeTest

--- src/test/java/com/cristianpeter/btree/core/BTreeNodeTest.java
+++ src/test/java/com/cristianpeter/btree/core/BTreeNodeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Named.named;
 
 import java.util.stream.Stream;
 
@@ -18,15 +19,21 @@ class BTreeNodeTest {
     public static class BTreeNodeConstants {
         private static final int VALUE = 10;
         private static final int ORDER = 4;
-        private static final BTreeNode leftChild = new BTreeNode(BTreeNodeConstants.ORDER, null);
-        private static final BTreeNode rightChild = new BTreeNode(BTreeNodeConstants.ORDER, null);
+        private static final BTreeNode ROOT_NODE_1 = new BTreeNode(BTreeNodeConstants.ORDER, null);
+        private static final BTreeNode ROOT_NODE_2 = new BTreeNode(BTreeNodeConstants.ORDER, null);
+        private static final BTreeNode ROOT_NODE_3 = new BTreeNode(BTreeNodeConstants.ORDER, null);
+
+        private static final BTreeNode LEAF_NODE_1 = new BTreeNode(BTreeNodeConstants.ORDER, ROOT_NODE_1);
+        private static final BTreeNode LEAF_NODE_2 = new BTreeNode(BTreeNodeConstants.ORDER, ROOT_NODE_2);
+        private static final BTreeNode LEAF_NODE_3 = new BTreeNode(BTreeNodeConstants.ORDER, ROOT_NODE_3);
+
     }
 
     public static class ArgumentsProvider {
 
-        static Stream<Arguments> provideChildrenWithExpectations() {
-            BTreeNode existingChild = BTreeNodeConstants.rightChild;
-            BTreeNode leftChild = BTreeNodeConstants.leftChild;
+        public static Stream<Arguments> provideChildrenWithExpectations() {
+            BTreeNode existingChild = BTreeNodeConstants.ROOT_NODE_2;
+            BTreeNode leftChild = BTreeNodeConstants.ROOT_NODE_1;
 
             return Stream.of(
                     Arguments.of(existingChild, leftChild, leftChild, existingChild),
@@ -34,6 +41,45 @@ class BTreeNodeTest {
                     Arguments.of(null, leftChild, leftChild, null)
             );
         }
+
+        public static Stream<Arguments> provideNodeAndOverflowingExpectation(){
+
+            BTreeNode leftChild = BTreeNodeConstants.ROOT_NODE_1;
+
+            // node is overflowing
+            leftChild.addKey(BTreeNodeConstants.VALUE);
+            leftChild.addKey(BTreeNodeConstants.VALUE + 1);
+            leftChild.addKey(BTreeNodeConstants.VALUE + 2);
+            leftChild.addKey(BTreeNodeConstants.VALUE + 3);
+            leftChild.addKey(BTreeNodeConstants.VALUE + 4);
+
+            return Stream.of(
+                    Arguments.of(leftChild, true),
+                    Arguments.of(BTreeNodeConstants.ROOT_NODE_2, false)
+            );
+        }
+
+        public static Stream<Arguments> provideNodeAndUnderflowingExpectation(){
+
+            BTreeNode normal = BTreeNodeConstants.LEAF_NODE_1;
+            normal.addKey(BTreeNodeConstants.VALUE);
+            normal.addKey(BTreeNodeConstants.VALUE + 1);
+            normal.addKey(BTreeNodeConstants.VALUE + 2);
+
+            BTreeNode upperMinimum = BTreeNodeConstants.LEAF_NODE_2;
+            upperMinimum.addKey(BTreeNodeConstants.VALUE);
+            upperMinimum.addKey(BTreeNodeConstants.VALUE + 1);
+
+            BTreeNode underMinimun = BTreeNodeConstants.LEAF_NODE_3;
+            underMinimun.addKey(BTreeNodeConstants.VALUE);
+
+            return Stream.of(
+                    Arguments.of(normal, false),
+                    Arguments.of(upperMinimum, false),
+                    Arguments.of(underMinimun, true)
+            );
+        }
+
     }
 
     @Test
@@ -71,4 +117,18 @@ class BTreeNodeTest {
                 () -> assertEquals(expectedAt1, parent.getChild(1)));
     }
 
+
+    @ParameterizedTest
+    @DisplayName("Add a single key to BTreeNode")
+    @MethodSource(value = "com.cristianpeter.btree.core.BTreeNodeTest$ArgumentsProvider#provideNodeAndOverflowingExpectation")
+    void keysOverflowingTest(BTreeNode node, boolean expected) {
+        assertEquals(expected, node.keysOverflowing());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Add a single key to BTreeNode")
+    @MethodSource(value = "com.cristianpeter.btree.core.BTreeNodeTest$ArgumentsProvider#provideNodeAndUnderflowingExpectation")
+    void keysUnderflowTest(BTreeNode node, boolean expected) {
+        assertEquals(expected, node.keysUnderflowing());
+    }
 }
